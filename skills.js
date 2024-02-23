78948b048d535a38779ef4bd1d8844292bc62410
askiad <askiad@stanford.edu>
2024-02-23 14:57:47 -0800

    random code

--- skills.js
+++ skills.js
@@ -1,4 +1,25 @@
 function calculateNumbers(var1, var2) {
     console.log(var1 + var2);
 }
-  
+
+function calculateProduct(var1, var2) {
+    console.log(var1 * var2);
+}
+
+function calculateDivision(var1, var2) {
+    console.log(var1 / var2);
+}
+
+module.exports = {
+    calculateNumbers: calculateNumbers,
+    calculateProduct: calculateProduct,
+    calculateDivision: calculateDivision
+}
+
+// Path: index.js
+var skills = require('./skills.js');
+
+skills.calculateNumbers(2, 3);
+skills.calculateProduct(2, 3);
+skills.calculateDivision(2, 3);
+
